3ed4713b42d0ae42e8911e6ce9aa62134cc32813
xingxingmofashu <xingxingmofashu@outlook.com>
2024-07-22 15:26:14 +0800

    chore tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: '~/tailwind.config.ts'
+  }
+})
