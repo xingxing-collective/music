f4cd306f59de0191422c2ce7d7e3742f952aa56f
星星魔法术 <xingxingmofashu@outlook.com>
2024-07-24 16:20:05 +0800

    feat carousel and pages (#2)
    * feat carousel-3d
    
    * feat routes
    
    * fix lock

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
-<script setup>
-const colorMode = useColorMode()
-</script>
-
 <template>
-   <div class="px-4 py-4">
-    
+  <div class="px-4 py-4">
+    <ClientOnly>
+      <carousel-3d style="margin: 0;" :perspective="0" :autoplay="true" :inverseScaling="150" :height="250" :width="700" :border="0" :loop="true" :space="400"
+        :display="3">
+        <slide v-for="(slide, i) in slides" :key="i" :index="i">
+          <div  class=" object-fill w-full h-full" alt="">{{ i }}</div>
+        </slide>
+      </carousel-3d>
+    </ClientOnly>
   </div>
 </template>
+<script setup lang="ts">
+import { Carousel3d, Slide } from 'vue3-carousel-3d'
+import "vue3-carousel-3d/dist/index.css"
+
+const slides = Array.from({ length: 5 })
+</script>
